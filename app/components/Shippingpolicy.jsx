77fd0cfd1b6ec8113f247ef0af4f2cd6b5ee4d94
dicedmang0 <mggibran@gmail.com>
2024-03-13 12:39:17 +0700

    latest push for shipping policy

--- app/components/Shippingpolicy.jsx
+++ app/components/Shippingpolicy.jsx
@@ -1,25 +1,51 @@
-import React from 'react'
+import React from 'react';
 
 function Shippingpolicy() {
   return (
-    <div className='Return-Container'>
-        <div className='Return-Content'>
+    <div className="Return-Container">
+      <div className="Return-Content">
         <h1>SHIPPING POLICY</h1>
+        <br />
+        <h2 style={{marginBottom:"0px", textTransform:'uppercase'}}>
+          Processing Days
+        </h2>
         <br/>
-        <p>We hope you will love everything you purchase from us but if something isn't quite right, you have 3 days from the day the order has been received to make an exchange.</p>
+        <p>
+            ⁠Shipments will be processed from Monday to Friday.&nbsp;
+        </p>
         <br/>
-        <p>Please keep in mind that a package unboxing video will need to be sent to us prior to the exchange.</p>
         <br/>
-        <p>If you wish to exchange your purchased item you can email your order number to: omomailbox@gmail.com</p>
+        <h2 style={{marginBottom:"0px", textTransform:'uppercase'}}>
+          Order Placement Deadline
+        </h2>
         <br/>
-        <p>Items can only be exchanged into different sizes. All exchanges are subject to product availability.</p>
+        <p>
+            Orders placed after 5:00 PM (+7 GMT) will be processed on the next
+            following business day.
+        </p>
         <br/>
-        <p>We ask that items are returned clean, unworn, and in the same condition as when you received them. Any items that are not in a condition suitable for resale (i.e. used, washed, or damaged), will not be exchanged and will be sent back to you.</p>
         <br/>
-        <p>Exchanges for defective items will be sent without shipping cost.</p>
-        </div>
+        <h2 style={{marginBottom:"0px", textTransform:'uppercase'}}>
+          Shipping Confirmation
+        </h2>
+        <br/>
+        <p>
+            Once your order is processed and shipped, you will receive a
+            shipping confirmation email with tracking information.
+        </p>
+        <br/>
+        <br/>
+        <h2 style={{marginBottom:"0px", textTransform:'uppercase'}}>
+          Delivery Time
+        </h2>
+        <br/>
+        <p>
+            Please note that delivery times may vary based on your location and
+            the shipping method chosen during checkout.
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Shippingpolicy
+export default Shippingpolicy;
